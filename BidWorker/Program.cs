895a1bb74cffb2bde3f6db14dffc971817980ad6
Anders <90785699+lynge96@users.noreply.github.com>
2023-05-10 10:11:04 +0200

    Fil struktur oprettet + Dockerfile.

---
+++ BidWorker/Program.cs
@@ -0,0 +1,10 @@
+using BidWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();

9ae2b00912e4a9d8eb3234d085bc8359ef958823
Jacob Kaae <jacobkaae0511@gmail.com>
2023-05-26 09:28:20 +0200

    Test fixes

---
+++ BidWorker.Test/WorkerTest.cs
@@ -0,0 +1,25 @@
+using BidWorker.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BidWorker.Test;
+
+public class WorkerTest
+{
+
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+}
